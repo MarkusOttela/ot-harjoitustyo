425ec8952ca83c6ec59250cbb1e0486cacd21612
Markus Ottela <markus.ottela@helsinki.fi>
2023-03-17 14:01:29 +0200

    Tehtävä 6: Lisää yksikkötestit

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -1,9 +1,38 @@
 import unittest
+
 from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
+
+    ALKUSUMMA = 1000
+
     def setUp(self):
-        self.maksukortti = Maksukortti(1000)
+        self.maksukortti = Maksukortti(self.ALKUSUMMA)
 
     def test_luotu_kortti_on_olemassa(self):
-        self.assertNotEqual(self.maksukortti, None)
+        self.assertIsNotNone(self.maksukortti)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo, self.ALKUSUMMA)
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        lisattava = 1
+        self.maksukortti.lataa_rahaa(lisattava)
+        self.assertEqual(self.maksukortti.saldo, self.ALKUSUMMA+lisattava)
+
+    def test_saldo_vahenee_oikein_jos_rahaa_on_tarpeeksi(self):
+        vahennettava = self.ALKUSUMMA-1
+        self.maksukortti.ota_rahaa(vahennettava)
+        self.assertEqual(self.maksukortti.saldo, self.ALKUSUMMA-vahennettava)
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(self.ALKUSUMMA+1)
+        self.assertEqual(self.maksukortti.saldo, self.ALKUSUMMA)
+
+    def test_metodi_palauttaa_true_jos_rahat_riittivat_ja_muuten_false(self):
+        self.assertFalse(self.maksukortti.ota_rahaa(self.ALKUSUMMA+1))
+        self.assertTrue(self.maksukortti.ota_rahaa(self.ALKUSUMMA))
+
+
+if __name__ == '__main__':
+    unittest.main()

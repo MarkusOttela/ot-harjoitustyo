dae4debea1f7598b406db4cebe1442c1dea4e8aa
Markus Ottela <markus.ottela@helsinki.fi>
2023-05-14 01:14:17 +0300

    Start working on entities tests

--- tests/entities/test_ingredient.py
+++ tests/entities/test_ingredient.py
@@ -16,7 +16,65 @@ details. You should have received a copy of the GNU General Public License
 along with Calorinator. If not, see <https://www.gnu.org/licenses/>.
 """
 
-import typing
+import unittest
 
-if typing.TYPE_CHECKING:
-    pass
+from src.entities.ingredient import Ingredient
+from src.entities.nutritional_values import NutritionalValues
+
+
+class TestIngredient(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.ingredient1 = Ingredient('test_ingredient1', NutritionalValues())
+        self.ingredient2 = Ingredient('test_ingredient2', NutritionalValues())
+
+    def test_equality(self):
+        self.assertEqual(self.ingredient1, self.ingredient1)
+        self.assertNotEqual(self.ingredient1, self.ingredient2)
+
+    def test_str(self):
+        self.assertEqual(str(self.ingredient1), 'test_ingredient1')
+
+    def test_repr(self):
+        expected = f"""\
+<Ingredient-object {id(self.ingredient1)}>
+General Info
+  Name: test_ingredient1
+Nutrients: <NutritionalValues-object {id(self.ingredient1.nv_per_g)}>
+    Energy (per 1g of ingredient)
+      kcal            : 0.0
+    Macronutrients (per 1g of ingredient)
+      carbohydrates_g : 0.0
+      sugar_g         : 0.0
+      protein_g       : 0.0
+      fat_g           : 0.0
+      satisfied_fat_g : 0.0
+      fiber_g         : 0.0
+    Micronutrients (per 1g of ingredient)
+      salt_g          : 0.0
+      omega3_dha_mg   : 0.0
+      omega3_epa_mg   : 0.0
+      vitamin_a_ug    : 0.0
+      vitamin_d_ug    : 0.0
+      vitamin_e_mg    : 0.0
+      vitamin_k_ug    : 0.0
+      vitamin_b1_mg   : 0.0
+      vitamin_b2_mg   : 0.0
+      vitamin_b3_mg   : 0.0
+      vitamin_b5_mg   : 0.0
+      vitamin_b6_mg   : 0.0
+      vitamin_b7_ug   : 0.0
+      vitamin_b9_ug   : 0.0
+      vitamin_b12_ug  : 0.0
+      vitamin_c_mg    : 0.0
+      calcium_mg      : 0.0
+      chromium_ug     : 0.0
+      iodine_ug       : 0.0
+      potassium_mg    : 0.0
+      iron_mg         : 0.0
+      magnesium_mg    : 0.0
+      zinc_mg         : 0.0
+      caffeine_mg     : 0.0
+      creatine_g      : 0.0"""
+
+        self.assertEqual(repr(self.ingredient1), expected)

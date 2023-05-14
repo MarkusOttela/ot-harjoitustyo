8ca6ef738086249d78453e07de8ca13ab0bb9068
Markus Ottela <markus.ottela@helsinki.fi>
2023-05-14 16:41:05 +0300

    Remove some pylint warnings

--- src/ui/screens/statistics/daily_overview.py
+++ src/ui/screens/statistics/daily_overview.py
@@ -56,35 +56,35 @@ def get_meal_lines(user      : 'User',
     meal_list = user.get_todays_meals()
 
     # Columns
-    c1, c2, c3, c4, c5, c6, c7, c8 = ([] for _ in range(8))
+    col1, col2, col3, col4, col5, col6, col7, col8 = ([] for _ in range(8))
 
     if not meal_list:
         lines.append("    <n/a>")
     else:
         for meal in meal_list:
-            c1.append(len(meal.eat_time) * ' ')
-            c2.append(meal.eat_time)
-            c3.append(meal.name)
-            c4.append(meal.total_weight)
-            c5.append(meal.meal_nv.protein_g)
-            c6.append(meal.meal_nv.kcal)
-            c7.append(meal.meal_nv.carbohydrates_g)
-            c8.append(meal.meal_nv.fat_g)
+            col1.append(len(meal.eat_time) * ' ')
+            col2.append(meal.eat_time)
+            col3.append(meal.name)
+            col4.append(meal.total_weight)
+            col5.append(meal.meal_nv.protein_g)
+            col6.append(meal.meal_nv.kcal)
+            col7.append(meal.meal_nv.carbohydrates_g)
+            col8.append(meal.meal_nv.fat_g)
 
             total_consumed_kcal      += meal.meal_nv.kcal
             total_consumed_carbs_g   += meal.meal_nv.carbohydrates_g
             total_consumed_protein_g += meal.meal_nv.protein_g
             total_consumed_fat_g     += meal.meal_nv.fat_g
 
-        c4s = align_float(c4)
-        c5s = align_float(c5)
-        c6s = align_float(c6)
-        c7s = align_float(c7)
-        c8s = align_float(c8)
+        c4s = align_float(col4)
+        c5s = align_float(col5)
+        c6s = align_float(col6)
+        c7s = align_float(col7)
+        c8s = align_float(col8)
 
-        c1.insert(0, '        ')
-        c2.insert(0, 'Time')
-        c3.insert(0, 'Meal')
+        col1.insert(0, '        ')
+        col2.insert(0, 'Time')
+        col3.insert(0, 'Meal')
         c4s.insert(0, 'Serving (g)')
         c5s.insert(0, 'Protein (g)')
         c6s.insert(0, 'Energy (kcal)')
@@ -93,7 +93,8 @@ def get_meal_lines(user      : 'User',
 
         # Determine column widths
         c1w, c2w, c3w, c4w, c5w, c6w, c7w, c8w \
-            = [max(len(v) for v in column) for column in [c1, c2, c3, c4s, c5s, c6s, c7s, c8s]]
+            = [max(len(v) for v in col) for col in [col1, col2, col3,
+                                                    c4s, c5s, c6s, c7s, c8s]]
 
         recipe_names = [r[:17] for r in recipe_db.get_list_of_recipe_names()]
 
@@ -101,7 +102,8 @@ def get_meal_lines(user      : 'User',
 
         lines.extend([f'{f1:<{c1w}}  {f2:<{c2w}}  {f3:<{c3w}}  {f4:>{c4w}}  '
                       f'{f5:>{c5w}}  {f6:>{c6w}}  {f7:>{c7w}}  {f8:>{c8w}}'
-                      for f1, f2, f3, f4, f5, f6, f7, f8 in zip(c1, c2, c3, c4s, c5s, c6s, c7s, c8s)])
+                      for f1, f2, f3, f4, f5, f6, f7, f8 in zip(col1, col2, col3, c4s, 
+                                                                c5s, c6s, c7s, c8s)])
 
     return lines, total_consumed_kcal, total_consumed_carbs_g
 
@@ -134,32 +136,32 @@ def get_daily_macro_lines(user                 : 'User',
     carb_goal_g              = diet_carb_g_goal       + total_burned_carbs_g
 
     # Columns
-    c1 = ['Energy:', 'Carbs:', 'Protein:', 'Fat:']
+    col1 = ['Energy:', 'Carbs:', 'Protein:', 'Fat:']
 
-    c2 = align_float([total_consumed_kcal,
-                      total_consumed_carbs_g,
-                      total_consumed_protein_g,
-                      total_consumed_fat_g])
+    col2 = align_float([total_consumed_kcal,
+                        total_consumed_carbs_g,
+                        total_consumed_protein_g,
+                        total_consumed_fat_g])
 
-    c3 = align_float([kcal_goal,
-                      carb_goal_g,
-                      diet_prot_g_goal,
-                      diet_fat_g_goal])
+    col3 = align_float([kcal_goal,
+                        carb_goal_g,
+                        diet_prot_g_goal,
+                        diet_fat_g_goal])
 
-    c4 = ['kcal', 'g', 'g', 'g', 'g']
+    col4 = ['kcal', 'g', 'g', 'g', 'g']
 
-    c5 = align_float([(consumed / goal * 100.0) for consumed,
-                      goal in [(total_consumed_kcal,      kcal_goal       ),
-                               (total_consumed_carbs_g,   carb_goal_g     ),
-                               (total_consumed_protein_g, diet_prot_g_goal),
-                               (total_consumed_fat_g,     diet_fat_g_goal )] ])
+    col5 = align_float([(consumed / goal * 100.0) for consumed,
+                        goal in [(total_consumed_kcal,      kcal_goal       ),
+                                 (total_consumed_carbs_g,   carb_goal_g     ),
+                                 (total_consumed_protein_g, diet_prot_g_goal),
+                                 (total_consumed_fat_g,     diet_fat_g_goal )] ])
 
     # Column widths
-    c1w, c2w, c3w, c4w = [max(len(v) for v in column) for column in [c1, c2, c3, c4]]
+    c1w, c2w, c3w, c4w = [max(len(v) for v in col) for col in [col1, col2, col3, col4]]
 
     lines = [f'          {f1:{c1w}} {f2:{c2w}} / {f3:{c3w}} {f4:{c4w}} ({f5}% of daily goal) '
              f'{"!" if float(f5) > 100.0 else ""}'
-             for f1, f2, f3, f4, f5 in zip(c1, c2, c3, c4, c5)]
+             for f1, f2, f3, f4, f5 in zip(col1, col2, col3, col4, col5)]
 
     return ['', 'Macros:'] + lines
 
@@ -184,14 +186,15 @@ def get_calorie_balance(user: 'User') -> list:
 
     total_burned_g   = bmr_total_g
     total_consumed_g = consumed_carbs_g + consumed_fats_g
-    weight_change_g  = total_consumed_g - total_burned_g
-    weight_change_kg = weight_change_g / 1000
+
+    w_change_g  = total_consumed_g - total_burned_g
+    w_change_kg = w_change_g / 1000
 
     kcal_change     = consumed_kcal - bmr_kcal
     current_weight  = user.get_todays_weight()
-    weight_tomorrow = current_weight + weight_change_kg
+    weight_tomorrow = current_weight + w_change_kg
 
-    consumed_kcal_aligned, bmd_kcal_aligned, kcal_change_aligned \
+    consumed_kcal_al, bmd_kcal_al, kcal_change_al \
         = align_float([consumed_kcal, bmr_kcal, kcal_change])
 
     bmr_warning = f'! Below BMR' if consumed_kcal < bmr_kcal else ''
@@ -203,39 +206,44 @@ def get_calorie_balance(user: 'User') -> list:
     carb_total_change_g = consumed_carbs_g - bmr_carbs_g
     fat_total_change_g  = consumed_fats_g  - bmr_fat_g
 
-    weight_change_dir = '+' if weight_change_g     >= 0 else ''
+    w_change_dir = '+' if w_change_g     >= 0 else ''
     carb_change_dir   = '-' if carb_total_change_g  < 0 else '+'
     fat_change_dir    = '-' if fat_total_change_g   < 0 else '+'
 
     # Columns
-    c0 = ['', '', '']
-    c1 = ['Food-TEF', 'BMR']
-    c2 = ['+', '-', '-']
-    c3 = [consumed_kcal_aligned, bmd_kcal_aligned]
-    c4 = ['C +', 'C -', 'C -']
-    c5 = [consumed_carbs_g_aligned, bmr_carbs_g_aligned]
-    c6 = ['g', 'g', 'g']
-    c7 = ['F +', 'F -', 'F -']
-    c8 = [consumed_fat_g_aligned, bmr_fat_g_aligned]
-    c9 = ['g', 'g', 'g']
-    c10 = [bmr_warning, '', '']
-    c11 = [consumed_total_g_aligned, bmr_total_g_aligned]
+    col0 = ['', '', '']
+    col1 = ['Food-TEF', 'BMR']
+    col2 = ['+', '-', '-']
+    col3 = [consumed_kcal_al, bmd_kcal_al]
+    col4 = ['C +', 'C -', 'C -']
+    col5 = [consumed_carbs_g_aligned, bmr_carbs_g_aligned]
+    col6 = ['g', 'g', 'g']
+    col7 = ['F +', 'F -', 'F -']
+    col8 = [consumed_fat_g_aligned, bmr_fat_g_aligned]
+    col9 = ['g', 'g', 'g']
+    col10 = [bmr_warning, '', '']
+    col11 = [consumed_total_g_aligned, bmr_total_g_aligned]
 
     # Column widths
     c0w, c1w, c2w, c3w, c4w, c5w, c6w, c7w, c8w, c9w \
-        = [max(len(v) for v in column) for column in [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9]]
+        = [max(len(v) for v in col) for col in [col0, col1, col2, col3, col4,
+                                                col5, col6, col7, col8, col9]]
 
     lines = ['', 'Daily calorie balance:']
 
     lines += [
         f'         {f0} {f1:{c1w}} {f2:{c2w}} {f3:{c3w}} kcal'
-        f'           {f2}  {f11:4}g  ({f4:{c4w}} {f5:{c5w}}{f6:{c6w}}  {f7:{c7w}} {f8:{c8w}}{f9:{c9w}}) {f10}'
-        for f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11 in zip(c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11)]
+        f'           {f2}  {f11:4}g  ({f4:{c4w}} {f5:{c5w}}{f6:{c6w}}  '
+        f'{f7:{c7w}} {f8:{c8w}}{f9:{c9w}}) {f10}'
+        for f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11 in zip(col0, col1, col2, col3, col4,
+                                                                    col5, col6, col7, col8, col9,
+                                                                    col10, col11)]
 
-    lines += [f'{19 * " " + 14 * "─" + 2 * " " + 18 * "─" + 2 * " " + 11 * "─" + 2 * " " + 11 * "─"}',
-              f'                   = {kcal_change_aligned} kcal  D weight = {weight_change_dir}{weight_change_g:.1f}g'
-              f'  (C {carb_change_dir} {abs(carb_total_change_g):.1f}g  F {fat_change_dir} '
-              f'{abs(fat_total_change_g):.1f}g) ({current_weight}kg -> {weight_tomorrow:.1f}kg)']
+    lines += \
+        [f'{19 * " " + 14 * "─" + 2 * " " + 18 * "─" + 2 * " " + 11 * "─" + 2 * " " + 11 * "─"}',
+         f'                   = {kcal_change_al} kcal  D weight = {w_change_dir}{w_change_g:.1f}g'
+         f'  (C {carb_change_dir} {abs(carb_total_change_g):.1f}g  F {fat_change_dir} '
+         f'{abs(fat_total_change_g):.1f}g) ({current_weight}kg -> {weight_tomorrow:.1f}kg)']
 
     return lines
 
